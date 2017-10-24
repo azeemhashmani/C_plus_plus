fe79a8cb05974b0cd4fbb348121a04e15a31e0df
azeemhashmani <azeemhashmani@gmail.com>
2017-10-24 19:29:01 -0400

    How to sprintf string class

---
+++ strings/stringClassSprintf.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    char ipAddr[12] = {'\0'};
+    int  incr = 125;
+
+    incr++;
+
+    sprintf(ipAddr, "10.86.%d.0", incr);
+
+    string sIpAddr(&ipAddr[0]);
+
+    cout << sIpAddr << endl;
+
+   return 0;
+
+}
