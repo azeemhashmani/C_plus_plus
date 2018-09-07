5cfa44821b009c57e947e143a47c2aaae567ac3c
Azeem Ali <azeemhashmani@gmail.com>
2018-09-07 00:44:59 -0400

    Print a line without semi-colon in C/C++

---
+++ Interview_Questions/Programs_With_Conditions/print_without_semicolon.c
@@ -0,0 +1,106 @@
+/* Write a program to print a data without using semi-colon in C/C++
+ */
+
+Method 1
+- Using if statement
+
+/* Include header files
+ */
+#include <stdio.h>
+
+int main()
+{
+    if(printf("Hello World !"))
+    {
+
+    }
+
+    return 0;
+}
+
+
+
+Method 2
+- Using else if statement
+
+/* Include header files
+ */
+#include <stdio.h>
+
+int main()
+{
+    if(0)
+    {
+
+    }
+    else if (printf("Hello World !"))
+    {
+
+    }
+    return 0;
+}
+
+
+
+Method 3
+- Using for statement
+
+
+/* Include header files
+ */
+#include <stdio.h>
+
+int main()
+{
+    for (printf("Hello World !") )
+
+    {
+
+
+    }
+
+    return 0;
+}
+
+
+
+Method 4
+- Using while statement
+
+
+/* Include header files
+ */
+#include <stdio.h>
+
+
+
+int main()
+{
+    while (!printf("Hello World !"))
+    {
+
+
+    }
+
+    return 0;
+}
+
+
+Method 5
+- Using switch statement
+
+
+/* Include header files
+ */
+#include <stdio.h>
+
+int main()
+{
+    switch(printf("Hello World !"))
+    {
+
+
+    }
+    
+    return 0;
+}
